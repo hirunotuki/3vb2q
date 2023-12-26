6eab2d775e25946c62a97cb88632523f92ac5d92
kanwakyudai <129352599+kanwakyudai@users.noreply.github.com>
2023-12-27 02:59:02 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -40,21 +40,63 @@ $(() => {
   })
 })
 
-//header transition
-// $(() => {
-//   // let cawTop = $('#codeAreaContainer').offset().top;
-//   let introH = $('#introduction').height();
-//   $('main').on('scroll', () => {
-//     let scroll = $('main').scrollTop();
-//     if (scroll >= introH) {
-//       $('header').addClass('whileCode');
-//       $('main').addClass('whileCode');
-//       // cawTop = $('#codeAreaContainer').position().top;
-//     }
-//     else {
-//       $('header').removeClass('whileCode');
-//       $('main').removeClass('whileCode');
-//       // cawTop = $('#codeAreaContainer').position().top;
-//     }
-//   })
-// })
+
+$(() => {
+  $(window).on('load', () => {
+    colW = $('.js-card').outerWidth() + offsetX * 2;
+    gridArray = [];
+    for (var i = 0; i < numOfCol; i++) {
+      pushGridArray(i, 0, -offsetY);
+    }
+    $('.js-card').each(function (index) {
+      setPosition($(this));
+    });
+  })
+})
+var gridArray = [],
+  colWidth,
+  offsetX = 5,
+  offsetY = 5,
+  numOfCol = 5;
+
+function pushGridArray(x, y, height) {
+  var grid = {};
+  grid.x = x;
+  grid.endY = y + height + offsetY * 2;
+  gridArray.push(grid);
+}
+function removeGridArray(x) {
+  var idx = getGridIndex(x);
+  gridArray.splice(idx, 1);
+}
+function getHeightArray(x) {
+  var heightArray = {};
+  var temps = [];
+  var idx = getGridIndex(x);
+  temps.push(gridArray[idx].endY);
+  heightArray.min = Math.min.apply(Math, temps);
+  heightArray.max = Math.max.apply(Math, temps);
+  heightArray.x = temps.indexOf(heightArray.min)
+  return heightArray
+}
+function getGridIndex(x) {
+  for (var i = 0; i < gridArray.length; i++) {
+    var obj = gridArray[i];
+    if (obj.x === x) {
+      return i;
+    }
+  }
+}
+function setPosition(grid) {
+  var pos = {};
+  var tempH = getHeightArray(0, gridArray.length);
+  pos.x = tempH.x;
+  pos.y = tempH.min;
+  grid.css({
+    'left': pos.x * colW,
+    'top': pos.y,
+    'position': 'absolute'
+  })
+  removeGridArray(pos.x, grid.data('size'));
+  pushGridArray(pos.x, pos.y, grid.data('size'), grid.outerHeight());
+}
